1a35f4eea2e9b0fc9ec38fc94ab8b17619211c7c
Jeph2001 <josephmaniragaba9@gmail.com>
2024-08-10 15:51:56 +0200

    tttt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "bucketname" {
    default = "myterraformprojectwebsite20011231"
 }
 
-variable "github_token" {
-  description = "GitHub access token"
-  type        = string
-}
+# variable "github_token" {
+#   description = "GitHub access token"
+#   type        = string
+# }
