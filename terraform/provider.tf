c95ab7e0255cf8460eebc2de54885ad3702bdc76
Jeph2001 <josephmaniragaba9@gmail.com>
2024-08-10 12:18:51 +0200

    fixing issues in configuration provider file of terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,23 +1,23 @@
- terraform {
-   required_providers {
-     aws = {
-        source = "hashicorp/aws"
-        version = "5.10.0"
-     }
-   }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
 
-   backend "s3" {
-     bucket = "myterraformprojectwebsite20011231"
-     key = "terraform/state.tfstate"
-     region = "us-west-2"
-   }
+  backend "s3" {
+    bucket = "myterraformprojectwebsite20011231"
+    key    = "terraform/state.tfstate"
+    region = "us-west-2"
+  }
 
- }
+}
 
- provider "aws" {
-   region = "us-west-2"
- }
+provider "aws" {
+  region = "us-west-2"
+}
 
- provider "github" {
-   token = var.github_token
- }
+provider "github" {
+  token = var.github_token
+}
