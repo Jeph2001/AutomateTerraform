03ce440bbaf70031619c0ec1825f09af18684c1d
Jeph2001 <josephmaniragaba9@gmail.com>
2024-08-10 12:04:54 +0200

    Update provider.tf to use environment variables for sensitive data

---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+ terraform {
+   required_providers {
+     aws = {
+        source = "hashicorp/aws"
+        version = "5.10.0"
+     }
+   }
+ }
+
+ terraform {
+   backend "s3" {
+     bucket = "myterraformprojectwebsite20011231"
+     key = "terraform/state.tfstate"
+     region = "us-west-2"
+   }
+ }
+
+ provider "aws" {
+   region = "us-west-2"
+ }
+
+ provider "github" {
+   token = var.github_token
+ }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
 variable "bucketname" {
    default = "myterraformprojectwebsite20011231"
 }
+
+variable "github_token" {
+  description = "GitHub access token"
+  type        = string
+  sensitive   = true
+}
